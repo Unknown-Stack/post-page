7139a06855153e3dcf2627805825163bcacd680d
Danil <iknownstack@gmail.com>
2021-08-16 23:55:55 +0300

    First commit

---
+++ src/components/post-list-item/post-list-item.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './post-list-item.css';
+
+const PostListItem = () => {
+    return (
+        <li className="app-list-item d-flex justify-content-between">
+            <span className="app-list-item-label">
+                Hello world!
+            </span>
+            <div className="d-flex justify-content-center align-items-center">
+                <button className="btn-star btn-sm">
+                    <i className="fas fa-star"></i>
+                </button>
+                <button className="btn-trash btn-sm">
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-heart"></i>
+            </div>
+        </li>
+    )
+}
+export default PostListItem;
