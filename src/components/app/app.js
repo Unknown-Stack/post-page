af28f721bb29451040fb5bef8d795f2ba81f8d36
Danil <iknownstack@gmail.com>
2021-08-17 21:27:41 +0300

    Like and star functional in post-list-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,13 @@ import PostAddForm from "../post-add-form";
 import './app.css';
 
 const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', id: 'qwe'},
+        {label: 'That is so good', id: 'qww'},
+        {label: 'I need a break', id: 'wwq'}
+    ];
+
     return (
         <div className="app">
             <AppHeader/>
@@ -15,7 +22,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
 
