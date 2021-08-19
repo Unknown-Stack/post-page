983b2027a2de9b6a7ece1152f676f5fb60f48200
Danil <iknownstack@gmail.com>
2021-08-19 17:21:53 +0300

    Search panel functional, adding post add form functional

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,6 +8,7 @@ import PostAddForm from "../post-add-form";
 import './app.css';
 import styled from 'styled-components';
 
+
 const AppBlock = styled.div`
   max-width: 800px;
   margin: 2rem auto 0;
@@ -22,15 +23,19 @@ export default class App extends  Component {
         super(props);
         this.state = {
             data : [
-                {label: 'Going to learn React', important: true, like:false, id: 1},
-                {label: 'That is so good', important: true, like:true, id: 2},
-                {label: 'I need a break', important: true, like:false, id: 3}
-            ]
+                {label: 'Going to learn React', important: false, like:false, id: 1},
+                {label: 'That is so good', important: false, like:false, id: 2},
+                {label: 'I need a break', important: false, like:false, id: 3}
+            ],
+            term:'',
+            filter: 'all'
         };
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggledLike = this.onToggledLike.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
         this.maxId = 4;
     }
     deleteItem(id) {
@@ -81,19 +86,41 @@ export default class App extends  Component {
             }
         })
     }
+    searchPost(items, term) {
+        if (term.length === 0) {
+            return items
+        }
+        return  items.filter(item => {
+            return item.label.indexOf(term) > -1
+        })
+    }
+    onUpdateSearch(term) {
+        this.setState({term});
+    }
+    filterPost(items, filter) {
+        if (filter === 'like') {
+            return items.filter(item => item.like)
+        } else {
+            return items
+        }
+    }
+    onFilterSelect (filter) {
+        this.setState({filter})
+    }
     render() {
-        const {data} = this.state;
+        const {data, term, filter} = this.state;
         const liked = data.filter(item=> item.like).length;
         const allPosts =data.length;
+        const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
         return (
             // <StyledAppBlock>
             <AppBlock>
                 <AppHeader liked={liked} allPosts={allPosts}/>
                 <div className="search-panel d-flex">
-                    <SearchPanel/>
-                    <PostStatusFilter/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
+                    <PostStatusFilter filter={filter} onFilterSelect={this.onFilterSelect}/>
                 </div>
-                <PostList posts={data} onDelete={this.deleteItem} onToggleImportant={this.onToggleImportant}
+                <PostList posts={visiblePosts} onDelete={this.deleteItem} onToggleImportant={this.onToggleImportant}
                 onToggledLike={this.onToggledLike}/>
                 <PostAddForm onAdd={this.addItem}/>
             </AppBlock>
